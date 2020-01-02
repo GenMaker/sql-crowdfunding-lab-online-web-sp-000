39dc996d3195c31d1c7307ced2d1257915b08385
Panela Leung <leungpanela@gmail.com>
2020-01-02 04:07:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -26,7 +26,7 @@ VALUES ('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
 VALUES ('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
-VALUES (Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
+VALUES ('Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
 VALUES ('Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
@@ -36,7 +36,7 @@ VALUES ('Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-1
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
 VALUES ('I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
-VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
+VALUES ('I want to teach English in China', 'charity', 3000.00, '2013-06-30');
 INSERT INTO projects (title, catagory, funding_goal, start_date, end_date)
 VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
 
