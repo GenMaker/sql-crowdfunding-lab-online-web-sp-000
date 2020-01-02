d3ee5f900a391f9d735978f968a12a8d2e851477
Panela Leung <leungpanela@gmail.com>
2020-01-02 03:39:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,6 +19,7 @@ INSERT INTO users (name,age) VALUES ();
 INSERT INTO users (name,age) VALUES ();
 INSERT INTO users (name,age) VALUES ();
 
-INSERT INTO projects(title,)
+INSERT INTO projects(title, catagory, funding_goal, start_date, end_date)
+VALUES ();
 
 INSERT INTO pledges
