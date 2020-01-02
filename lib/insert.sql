999c66aef6f512245bf154bc990a2b5cc495ef28
Panela Leung <leungpanela@gmail.com>
2020-01-02 04:20:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -69,6 +69,6 @@ INSERT INTO pledges (amount, user_id, project_id) VALUES (70.00, 20, 10);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100.00, 20, 4);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (40.00, 19, 1);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (20.00, 18, 6);
-INSERT INTO pledges (amount, user_id, project_id) VALUES ();
+INSERT INTO pledges (amount, user_id, project_id) VALUES (90.00, 17, 9);
 INSERT INTO pledges (amount, user_id, project_id) VALUES ();
 INSERT INTO pledges (amount, user_id, project_id) VALUES ();
