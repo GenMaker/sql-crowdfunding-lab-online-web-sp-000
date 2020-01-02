f62f54676ea2ae03534c3c22230989860ec12dac
Panela Leung <leungpanela@gmail.com>
2020-01-02 05:03:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,9 +37,10 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT project.category, SUM(pledges.amount) FROM projects
+  "SELECT project.category, pledges.amount FROM projects
   INNER JOIN pledges
-  ON pledges.= "
+  ON project_id = pledges.projects_id
+  WHERE catagory = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
