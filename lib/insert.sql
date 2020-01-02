21667fa9a4a0f2704c5a06e2aaec00208193a9b1
Panela Leung <leungpanela@gmail.com>
2020-01-02 03:37:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,23 @@
-INSERT INTO users (name,age)
-VALUES 
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
+INSERT INTO users (name,age) VALUES ();
 
 INSERT INTO projects
 
